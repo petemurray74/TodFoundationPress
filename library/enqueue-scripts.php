aef697faf0b6b8e15ff1d0340fee0a5d1500d1e0
petemurray74 <github@thrap.co.uk>
2016-02-21 22:29:29 +0000

    google fonts

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -14,6 +14,8 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 
 	// Enqueue the main Stylesheet.
 	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css', array(), '2.3.0', 'all' );
+	
+	wp_enqueue_style( 'googlefonts', '//fonts.googleapis.com/css?family=Raleway:400,700|Cabin:400,700' );
 
 	// Deregister the jquery version bundled with WordPress.
 	wp_deregister_script( 'jquery' );
@@ -21,6 +23,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
 	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
 
+		
 	// If you'd like to cherry-pick the foundation components you need in your project, head over to gulpfile.js and see lines 35-54.
 	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
 	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '2.3.0', true );
