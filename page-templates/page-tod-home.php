f3d1e496f742e1650f9d4994ab4c8d8876c69e40
petemurray74 <github@thrap.co.uk>
2016-09-19 21:01:59 +0100

    Signup form on home

--- page-templates/page-tod-home.php
+++ page-templates/page-tod-home.php
@@ -59,6 +59,39 @@ get_header(); ?>
 </div>
 </div>
 
+<div class="formrow">
+<div class="row home-signupform">  
+<div class="medium-12 columns">
+	<h3>Be the first to hear about concerts</h3>
+	<!-- Begin MailChimp Signup Form -->
+</div>	
+	<div id="mc_embed_signup">
+	<form action="//todmordenorchestra.us13.list-manage.com/subscribe/post?u=bf6c084daf544aa4029cba35c&amp;id=51f10e1f86" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+	<div id="mc_embed_signup_scroll">
+	<div class="mc-field-group">
+		<label for="mce-FNAME">First Name </label><input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+	</div>
+	<div class="mc-field-group">
+		<label for="mce-LNAME">Last Name </label><input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+	</div>
+	<div class="mc-field-group">
+		<label for="mce-EMAIL">Email Address</label><input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+	</div>
+	<div id="mce-responses" class="clear">
+	<div class="response" id="mce-error-response" style="display:none"></div>
+	<div class="response" id="mce-success-response" style="display:none"></div>
+	<p></p></div>
+	<p>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--></p>
+	<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_bf6c084daf544aa4029cba35c_51f10e1f86" tabindex="-1" value=""></div>
+	<div class="mc-field-group clear"><input type="submit" value="Keep me updated" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+	<p></p></div>
+	</form>
+	</div>
+	<!--End mc_embed_signup-->
+</div>
+</div>
+</div>
+
 <?php do_action( 'foundationpress_after_content' ); ?>
 
 </div>
